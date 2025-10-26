bb05f67b5bb735113ad0e32b96596a7f0c9d90af
Tobias <tobias.bp2004@gmail.com>
2025-10-26 16:18:08 -0300

    feat: server setup ready

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,35 @@
-import fastify from "fastify";
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors"
+import { validatorCompiler, serializerCompiler, jsonSchemaTransform } from 'fastify-type-provider-zod'
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { routes } from "./routes";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
 
-const server = fastify();
+const server = fastify().withTypeProvider<ZodTypeProvider>();
+
+// Set to use zod
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+// Allow to front end to access all routes
+server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifySwagger, {
+  openapi:{
+    info: {
+      title: "Typed API",
+      version: '1.0.0'
+    }
+  },
+  transform: jsonSchemaTransform
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
+})
+
+server.register(routes)
 
 server.listen({port: 3333}).then(() => {
   console.log("Server is running on http://localhost:3333");
