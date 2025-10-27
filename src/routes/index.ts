5ee087f86f65130e15e375eec0a092062f69a249
Tobias <tobias.bp2004@gmail.com>
2025-10-27 11:25:26 -0300

    feat: added aiRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import type { FastifyTypedInstance } from '../types'
 import { matchRoutes } from './matches';
 import { teamRoutes } from './teams';
+import { aiRoutes } from './predictions';
 
 export async function routes(server: FastifyTypedInstance) {
   server.register(matchRoutes);
   server.register(teamRoutes);
+  server.register(aiRoutes);
 }
