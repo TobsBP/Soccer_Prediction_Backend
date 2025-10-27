931dd658966bbbe9726b11fe6df83d6a6907335c
Tobias <tobias.bp2004@gmail.com>
2025-10-27 14:42:32 -0300

    feat: the ai predict now the result

--- src/resolvers/predictions.ts
+++ src/resolvers/predictions.ts
@@ -1,1 +1,16 @@
-// Logic to handle to storege the ai responses
+import { ai } from '../services/predictions';
+import { formatterMatches } from '../formatters/matches';
+
+export const askAI = async (homeTeamId: string, awayTeamId: string) => {
+  const prompt = await formatterMatches(homeTeamId, awayTeamId);
+
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.0-flash-001',
+    contents: [{ role: "user", parts: [{ text: prompt }] }],
+  });
+
+  // @ts-ignore
+  const text = response.candidates[0].content.parts[0].text;
+
+  return text;
+};
