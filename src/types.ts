813647278473d1efc251ef254981dd1c0d456773
Tobias <tobias.bp2004@gmail.com>
2025-10-26 22:27:45 -0300

    feat: new data formmat for matches

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,26 @@ export type FastifyTypedInstance = FastifyInstance<
   FastifyBaseLogger,
   ZodTypeProvider
 >
+
+export interface MatchData {
+  stage: {
+    matches: {
+      teams: {
+        home: {
+          id: number;
+          name: string;
+        };
+        away: {
+          id: number;
+          name: string;
+        };
+      };
+      goals: {
+        home_ft_goals: number;
+        away_ft_goals: number;
+      };
+      date: string;
+      time: string;
+    }[];
+  }[];
+}
