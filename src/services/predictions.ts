c1412c6724e8d2d1507c531677e7af3c60eb44bf
Tobias <tobias.bp2004@gmail.com>
2025-10-27 14:41:33 -0300

    feat: ai service ready

--- src/services/predictions.ts
+++ src/services/predictions.ts
@@ -1,1 +1,5 @@
-// Logic to handle the predictions
+import { GoogleGenAI } from "@google/genai";
+
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+export const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
